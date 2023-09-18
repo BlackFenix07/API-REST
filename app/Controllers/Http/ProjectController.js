1dfefb939a1ddce344d908b38a1a1b2557df53ab
Andrés Zapata <andresmauricio030400@gmail.com>
2023-09-18 16:52:03 -0500

    Clase de Método index

---
+++ app/Controllers/Http/ProjectController.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class ProjectController {
+  async index({ auth }) {
+    const user = await auth.getUser();
+    console.log(user);
+    return await user.projects().fetch();
+  }
+}
+
+module.exports = ProjectController;
